30565bdf558332475300ba97b5adf47ac72acfb8
Raghavg18 <graghav2006@gmail.com>
2025-07-08 23:10:27 +0530

    errorsolved

--- Socketio/Server.js
+++ Socketio/Server.js
@@ -9,7 +9,9 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://frontend-in9r1fbyz-raghavg18s-projects.vercel.app",
+        origin: ["https://frontend-in9r1fbyz-raghavg18s-projects.vercel.app",
+        "https://frontend-in9r1fbyz-raghavg18s-projects.vercel.app"],
+        credentials: true,
         methods: ["GET", "POST"],
     }
 });
